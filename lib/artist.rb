ee3c692fb2da69eee06e287f00dcdcc9af9485df
Kevin Chi <kevinchi118@gmail.com>
2018-06-21 01:06:14 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,7 +23,7 @@ class Artist
     end
 
     def self.find_or_create_by_name(name)
-        @@all.detect{|artist| artist.name == name} || artist = Artist.new(name).save
+        @@all.detect{|artist| artist.name == name} || artist = Artist.new(name)
     end
 
     def print_songs
