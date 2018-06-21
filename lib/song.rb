f9ecd6caa5078f4202997528504d9b5ddc9ed833
Kevin Chi <kevinchi118@gmail.com>
2018-06-21 01:25:19 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,7 @@ class Song
     def self.new_by_filename(file)
         split_name = file.split(" - ")
         song = Song.new(split_name[1])
+        artist = Artist.find_or_create_by_name(split_name[0])
         
 
     end
