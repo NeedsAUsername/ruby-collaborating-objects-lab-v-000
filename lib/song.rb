5d69e9e1a727b13d3ca0f8a6a43d754843987636
Kevin Chi <kevinchi118@gmail.com>
2018-06-21 01:25:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,6 +9,7 @@ class Song
         split_name = file.split(" - ")
         song = Song.new(split_name[1])
         artist = Artist.find_or_create_by_name(split_name[0])
+        song.artist = artist
         
 
     end
