cb08f3202f7bacae53df2f2949534ead0db7b0f8
Kevin Chi <kevinchi118@gmail.com>
2018-06-21 01:00:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,7 @@ class Artist
         @@all
     end
 
-    def find_or_create_by_name(name)
+    def self.find_or_create_by_name(name)
         if @@all.include?(name)
             @@all.detect{|artist| artist == name}
         else artist = Artist.new(name)
