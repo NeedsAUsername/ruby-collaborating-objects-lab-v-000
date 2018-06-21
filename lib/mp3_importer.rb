c010a3d2dccafbb26472619fa821040d8f1c06ae
Kevin Chi <kevinchi118@gmail.com>
2018-06-21 01:20:40 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,15 +1,17 @@
 class MP3Importer
+    attr_accessor :path
 
-  attr_accessor :path
+    def initialization(path)
+        @path = path
+    end
 
-  def initialize(path)
-    @path = path
-  end
+    def files
+        @files = Dir.entries(@path)
+        @files.delete_if {|file| file == "." || file == ".."}
+        #@files.delete_if {|file| !file.end_with?(".mp3")}
+    end
 
-  def files
-    @files = Dir.entries(@path)
-    @files.delete_if {|file| file == "." || file == ".."}
-  end
+    def import
+    end
 
-  
 end
