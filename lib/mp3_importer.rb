47e688ff24a877a56022b592d5bcfc7e7ecaf441
Kevin Chi <kevinchi118@gmail.com>
2018-06-21 01:20:31 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -11,10 +11,5 @@ class MP3Importer
     @files.delete_if {|file| file == "." || file == ".."}
   end
 
-  def import
-    self.files.each do |file|
-      song = Song.new_by_filename(file)
-      Artist.all << song.artist unless Artist.all.include?(song.artist)
-    end
-  end
+  
 end
